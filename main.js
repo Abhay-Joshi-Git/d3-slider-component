462bd5c504a66f0d80b597fd937c44e6b4780a39
abhay-joshi <abhay.joshi@drillinginfo.com>
2016-11-21 23:15:07 +0530

    changes done : 1 level

--- main.js
+++ main.js
@@ -2,20 +2,49 @@ var app = angular.module('app', []);
 
 app.controller('mainController', ['$scope', function($scope) {
   $scope.value1 = 30;
-  $scope.value2 = 26;
+  $scope.value2 = 30;
   $scope.value3 = 22;
   $scope.value4 = 18;
 
-  $scope.$watch('value1', function(newVal){
-    $scope.value2 = newVal - 4;
-    $scope.value3 = newVal - 8;
-    $scope.value4 = newVal - 12;
+  $scope.minValue1 = 25;
+  $scope.minValue2 = 15;
+  $scope.minValue3 = 2;
+  $scope.minValue4 = 1;
+
+  $scope.containerStyleOptions = {
+    height: '40px',
+    width: '150px',
+    margin: '10px 0'
+  };
+
+  $scope.secondValueWidth = '150px';
+
+  $scope.$watch('secondValueWidth', function(newVal, oldVal){
+    $scope.containerStyleOptions = {
+      height: '20px',
+      width: newVal,
+      margin: '10px 0'
+    };
   });
 
+  $scope.verticalLinesOption = {
+    verticalLinesColor: '#7f7f7f'
+  };
 
+  $scope.changeVerticalLinesColor = function() {
+    $scope.verticalLinesOption = {
+      verticalLinesColor: 'red'
+    };
+  };
+
+  //$scope.$watch('value1', function(newVal){
+  //  $scope.value2 = newVal - 4;
+  //  $scope.value3 = newVal - 8;
+  //  $scope.value4 = newVal - 12;
+  //});
 
   $scope.minValue = 10;
-  $scope.maxValue = 50;
+  $scope.maxValue = 35;
   $scope.valueChangeHandler = function(newValue) {
     console.log('value is changed, new value : ' + newValue, $scope.value1, $scope.value2, $scope.value3, $scope.value4);
   }
--- unitsSlider.js
+++ unitsSlider.js
@@ -6,96 +6,202 @@ angular.module('app')
         replace: true,
         template: '<div class="slider-container"></div>',
         scope: {
-          width: '=',
-          height: '=',
-
           value: '=',
           minValue: '=',
           maxValue: '=',
-
           onValueChange: '&',
-
-          verticalLineSeparationOptions: '=',
-
-          styleOptions: '='
+          containerStyleOptions: '=',
+          verticalLineOptions: '=',
+          sliderImageOptions: '=',
+          rectStyleOptions: '=',
+          maxPixelUnitsRatio: '='
         },
-        link: function(scope, element) {
-          element[0].style.height = element.parent()[0].offsetHeight + 'px';
-          var values = {
+        link: function(scope, element, attr) {
+          applyContainerStyle(scope, element);
+          var slider = Slider(getSliderConfig());
+
+          slider.draw({
             value: scope.value,
             minValue: scope.minValue,
             maxValue: scope.maxValue
-          };
-          var slider = Slider({
-            container: element[0],
-            slideHandler: slideHandler
           });
-          function slideHandler (newValue) {
-            scope.value = newValue;
-            scope.$apply();
-            if (typeof scope.onValueChange === 'function') {
-              scope.onValueChange({value: newValue});
-            }
-          }
-          slider.draw(values);
 
-          scope.$watch('value', function(changed, oldValue) {
-            if (!angular.equals(changed, oldValue)) {
+          scope.$watchGroup(['value', 'minValue', 'maxValue'], function(newValue, oldValue) {
+            if (!angular.equals(newValue, oldValue)) {
               slider.update({
-                value: changed,
+                value: scope.value,
                 minValue: scope.minValue,
                 maxValue: scope.maxValue
               });
             }
-          })
+          });
+
+          scope.$watch('containerStyleOptions', function(newValue, oldValue) {
+            if (!angular.equals(newValue, oldValue)) {
+              applyContainerStyle(scope, element);
+              refreshSlider();
+            }
+          });
+
+          scope.$watchGroup(['verticalLineOptions', 'sliderImageOptions', 'rectStyleOptions'], function(newVal, oldVal) {
+            if (!angular.equals(newVal, oldVal)) {
+              refreshSlider();
+            }
+          });
+
+          function slideHandler (newValue) {
+            scope.$evalAsync(function() {
+              scope.value = newValue
+            });
+            if (typeof scope.onValueChange === 'function') {
+              scope.onValueChange({value: newValue});
+            }
+          }
+
+          function refreshSlider() {
+            slider.refresh(getSliderConfig(), {
+              value: scope.value,
+              minValue: scope.minValue,
+              maxValue: scope.maxValue
+            });
+          }
+
+          function getSliderConfig() {
+            return {
+              container: element[0],
+              slideHandler: slideHandler,
+              verticalLineOptions: scope.verticalLineOptions,
+              sliderImageOptions: scope.sliderImageOptions,
+              rectStyleOptions: scope.rectStyleOptions,
+              maxPixelUnitsRatio: scope.maxPixelUnitsRatio
+            }
+          }
         }
       };
 
-      //TODO - vertical lines support
-      //TODO - width, height support, change in width & height
-      //TODO - style options support
+      function applyContainerStyle(scope, element) {
+        var ele = element[0];
+        ele.style.height = element.parent()[0].offsetHeight + 'px';
+        ele.style.width = '100%';
+        if (scope.containerStyleOptions) {
+          if (scope.containerStyleOptions.height) {
+            ele.style.height = scope.containerStyleOptions.height;
+          }
+          if (scope.containerStyleOptions.width) {
+            ele.style.width = scope.containerStyleOptions.width;
+          }
+          if (scope.containerStyleOptions.padding) {
+            ele.style.padding = scope.containerStyleOptions.padding;
+          }
+          if (scope.containerStyleOptions.margin) {
+            ele.style.margin = scope.containerStyleOptions.margin;
+          }
+        }
+      }
+
       //TODO - add group for all things which appear and disappear based on drag or no-drag
       //TODO - put a max height for rect and everything actually
-      //TODO - add de-bounce ??
 
       function Slider(config) {
-        var min_max_rect_opacity = config.min_max_rect_opacity || 0.5,
-          min_max_rect_default_opacity = config.min_max_rect_default_opacity || 0,
-          padding_units = config.padding_units || 10, //value to keep some padding between slider min and max movement
-          ideal_pixel_units_ratio = config.ideal_pixel_units_ratio || 5,
-          slider_image_height = config.slider_image_height ||10,
-          slider_image_width = config.slider_image_width || 15,
-          slider_image_inside_rect_percentage = config.slider_image_inside_rect_percentage || 30,
-          rect_height_percentage = config.rect_height_percentage || '60',
-          rect_color = config.rect_color || 'pink',
-          min_max_rect_color = config.min_max_rect_color || 'grey',
-          slider_image_path = config.slider_image_path || "images/slider-pin-1.png";
-
         var svg, rect, rect_top,
           drag, dragPin,
           maxRect, minRect, minBracket, maxBracket,
+          rect_top_svg_height_ratio,
           actualPixelUnitsRatio, pixelUnitsRatio, unitsRange,
-          value, minValue, maxValue;
+          value, minValue, maxValue,
+          paddingUnits, maxPixelUnitsRatio, totalWidth, totalHeight,
+          minMaxRectVisibleOpacity, minMaxRectDefaultOpacity, rectHeightPercentage,
+          rectColor, minMaxRectColor,
+          sliderImageHeight, sliderImageWidth, sliderImageRectPercentage, sliderImagePath,
+          verticalLinesColor, verticalLinesSeparation,
+          container;
 
-        var rect_top_svg_height_ratio = (100 - rect_height_percentage) / 2 / 100;
+        function serRectProps(config) {
+          minMaxRectVisibleOpacity = 0.5;
+          minMaxRectDefaultOpacity = 0;
+          rectHeightPercentage = '60';
+          rectColor = 'pink';
+          minMaxRectColor = 'grey';
+          if (config.rectStyleOptions) {
+            if (config.rectStyleOptions.minMaxRectVisibleOpacity) {
+              minMaxRectVisibleOpacity = config.rectStyleOptions.minMaxRectVisibleOpacity;
+            }
+            if (config.rectStyleOptions.minMaxRectDefaultOpacity) {
+              minMaxRectDefaultOpacity = config.rectStyleOptions.minMaxRectDefaultOpacity;
+            }
+            if (config.rectStyleOptions.rectHeightPercentage) {
+              rectHeightPercentage = config.rectStyleOptions.rectHeightPercentage;
+            }
+            if (config.rectStyleOptions.rectColor) {
+              rectColor = config.rectStyleOptions.rectColor;
+            }
+            if (config.rectStyleOptions.minMaxRectColor) {
+              minMaxRectColor = config.rectStyleOptions.minMaxRectColor;
+            }
+          }
+        }
+        function setSliderImageProps(config) {
+          sliderImageHeight = 10;
+          sliderImageWidth = 15;
+          sliderImageRectPercentage = 30;
+          sliderImagePath = "images/slider-pin.png";
+          if (config.sliderImageOptions) {
+            if (config.sliderImageOptions.sliderImageHeight) {
+              sliderImageHeight = config.sliderImageOptions.sliderImageHeight;
+            }
+            if (config.sliderImageOptions.sliderImageWidth) {
+              sliderImageWidth = config.sliderImageOptions.sliderImageWidth;
+            }
+            if (config.sliderImageOptions.sliderImageRectPercentage) {
+              sliderImageRectPercentage = config.sliderImageOptions.sliderImageRectPercentage;
+            }
+            if (config.sliderImageOptions.sliderImagePath) {
+              sliderImagePath = config.sliderImageOptions.sliderImagePath;
+            }
+          }
+        }
+        function serVerticalLineProps(config) {
+          verticalLinesColor = 'pink';
+          verticalLinesSeparation = 10;
+          if (config.verticalLineOptions) {
+            if (config.verticalLineOptions.verticalLinesColor) {
+              verticalLinesColor = config.verticalLineOptions.verticalLinesColor;
+            }
+            if (config.verticalLineOptions.verticalLinesSeparation) {
+              verticalLinesSeparation = config.verticalLineOptions.verticalLinesSeparation;
+            }
+          }
+        }
+        function setProps(config) {
+          paddingUnits = config.paddingUnits || 10; //value to keep some padding between slider min and max movement
+          maxPixelUnitsRatio = config.maxPixelUnitsRatio || 5;
+          totalWidth = config.width || '100%';
+          totalHeight = config.height || '100%';
+          serRectProps(config);
+          serVerticalLineProps(config);
+          setSliderImageProps(config);
+          rect_top_svg_height_ratio = (100 - rectHeightPercentage) / 2 / 100;
+          container = d3.select(config.container);
+          container.select('svg').remove();
+          svg = container.append('svg')
+            .attr('height', totalHeight)
+            .attr('width', totalWidth);
 
-        var container = d3.select(config.container);
-        container.select('svg').remove();
-        svg = container.append('svg')
-          .attr('height', '100%')
-          .attr('width', '100%');
+          //svg dependent props
+          rect_top = parseInt(svg.style('height'), 10) * rect_top_svg_height_ratio;
+        }
 
-        rect_top = parseInt(svg.style('height'), 10) * rect_top_svg_height_ratio;
+        setProps(config);
 
         function calValues(values) {
           value = values.value || 0;
           minValue = values.minValue || 0;
           maxValue = values.maxValue || 0;
 
-          unitsRange = maxValue - minValue + padding_units;
+          unitsRange = maxValue - minValue + paddingUnits;
           actualPixelUnitsRatio = parseInt(svg.style('width'), 10) / unitsRange;
-          pixelUnitsRatio = (actualPixelUnitsRatio > ideal_pixel_units_ratio) ?
-            ideal_pixel_units_ratio: actualPixelUnitsRatio;
+          pixelUnitsRatio = (actualPixelUnitsRatio > maxPixelUnitsRatio) ?
+            maxPixelUnitsRatio: actualPixelUnitsRatio;
         }
 
         function draw(values) {
@@ -104,6 +210,7 @@ angular.module('app')
         }
 
         function doDraw() {
+          appendVerticalLines();
           rect = appendRect();
           minRect = appendMinRect();
           maxRect = appendMaxRect();
@@ -115,14 +222,26 @@ angular.module('app')
 
         function update(values) {
           value = values.value;
+          minValue = values.minValue;
+          maxValue = values.maxValue;
+
           //move rectangles accordingly
           updateRect();
           updateSliderPin();
         }
 
-        //this will be called when dimensions are changed
-        function refresh(options) {
+        //this will be called when dimensions or styles are changed
+        function refresh(config, values) {
+          setProps(config);
+          draw(values);
+        }
 
+        function getRectWidth() {
+          return (paddingUnits / 2 + (value - minValue)) * pixelUnitsRatio;
+        }
+
+        function getMaxRectWidth() {
+          return (paddingUnits / 2 + (maxValue - minValue)) * pixelUnitsRatio;
         }
 
         function appendRect() {
@@ -131,31 +250,31 @@ angular.module('app')
             .attr('class', 'actual-rect')
             .attr("x", 0)
             .attr("y", rect_top)
-            .attr("height", rect_height_percentage + '%')
-            .attr("width", value * pixelUnitsRatio)
-            .style('fill', rect_color);
+            .attr("height", rectHeightPercentage + '%')
+            .attr("width", getRectWidth())
+            .style('fill', rectColor);
         }
 
         function updateRect() {
           svg.select('.actual-rect')
-            .attr('width', value * pixelUnitsRatio);
+            .attr('width', getRectWidth());
         }
 
         function appendSliderPin() {
-          var y = parseInt(svg.style('height'), 10) / 2 - (slider_image_height / 2);
+          var y = parseInt(svg.style('height'), 10) / 2 - (sliderImageHeight / 2);
           svg.select('.slider-pin-image').remove();
           return svg.append("svg:image")
             .attr('class', 'slider-pin-image')
-            .attr('x', value * pixelUnitsRatio - (slider_image_width * slider_image_inside_rect_percentage / 100))
+            .attr('x', getRectWidth() - (sliderImageWidth * sliderImageRectPercentage / 100))
             .attr('y', y)
-            .attr('width', slider_image_width)
-            .attr('height', slider_image_height)
-            .attr("xlink:href", slider_image_path);
+            .attr('width', sliderImageWidth)
+            .attr('height', sliderImageHeight)
+            .attr("xlink:href", sliderImagePath);
         }
 
         function updateSliderPin() {
           svg.select('.slider-pin-image')
-            .attr('x', value * pixelUnitsRatio - (slider_image_width * slider_image_inside_rect_percentage / 100))
+            .attr('x', getRectWidth() - (sliderImageWidth * sliderImageRectPercentage / 100))
         }
 
         function appendMinRect() {
@@ -164,34 +283,34 @@ angular.module('app')
             .attr('class', 'min-rect')
             .attr("x", 0)
             .attr("y", rect_top)
-            .attr("height", rect_height_percentage + '%')
-            .attr("width", (minValue < padding_units / 2 ? padding_units / 2 : minValue) * pixelUnitsRatio)
-            .style('fill', min_max_rect_color)
-            .style('opacity', min_max_rect_default_opacity)
+            .attr("height", rectHeightPercentage + '%')
+            .attr("width", (paddingUnits / 2) * pixelUnitsRatio)
+            .style('fill', minMaxRectColor)
+            .style('opacity', minMaxRectDefaultOpacity)
         }
 
         function appendMaxRect() {
           svg.select('.max-rect').remove();
           return svg.append('rect')
             .attr('class', 'max-rect')
-            .attr("x",  maxValue * pixelUnitsRatio)
+            .attr("x",  getMaxRectWidth())
             .attr("y", rect_top)
-            .attr("height", rect_height_percentage + '%')
+            .attr("height", rectHeightPercentage + '%')
             .attr("width", '100%')
-            .style('fill', min_max_rect_color)
-            .style('opacity', min_max_rect_default_opacity)
+            .style('fill', minMaxRectColor)
+            .style('opacity', minMaxRectDefaultOpacity)
         }
 
         function appendMinBracket() {
           svg.select('.min-bracket-text').remove();
           return svg.append('text')
             .attr('class', 'min-bracket-text')
-            .attr("x", (minValue < padding_units / 2 ? padding_units / 2 : minValue) * pixelUnitsRatio)
+            .attr("x", (paddingUnits / 2) * pixelUnitsRatio)
             .attr("y", parseInt(svg.style('height'), 10) / 2)
             .text("[")
             .attr("text-anchor", "middle")
             .attr("dominant-baseline", "middle")
-            .style('opacity', min_max_rect_default_opacity)
+            .style('opacity', minMaxRectDefaultOpacity)
             .style('font-size', parseInt(svg.style('height'), 10) * 0.85)
         }
 
@@ -199,33 +318,38 @@ angular.module('app')
           svg.select('.max-bracket-text').remove();
           return svg.append('text')
             .attr('class', 'max-bracket-text')
-            .attr("x", maxValue * pixelUnitsRatio)
+            .attr("x", getMaxRectWidth())
             .attr("y", parseInt(svg.style('height'), 10) / 2)
             .text("]")
             .attr("text-anchor", "middle")
             .attr("dominant-baseline", "middle")
-            .style('opacity', min_max_rect_default_opacity)
+            .style('opacity', minMaxRectDefaultOpacity)
             .style('font-size', parseInt(svg.style('height'), 10) * 0.85)
         }
 
         function dragHandler () {
+          var slider_image_inside_rect = (sliderImageWidth * sliderImageRectPercentage / 100);
           var newX = +dragPin.attr('x') + d3.event.dx;
-          var slider_image_inside_rect = (slider_image_width * slider_image_inside_rect_percentage / 100);
+          newX = Number(newX.toFixed(2));
 
-          if ((newX > (maxValue * pixelUnitsRatio - slider_image_inside_rect) ) ||
-            newX < ((minValue < padding_units / 2 ? padding_units / 2 : minValue) * pixelUnitsRatio - padding_units / 2)) {
+          if ((newX > (getMaxRectWidth() - slider_image_inside_rect) ) ||
+            newX < ((paddingUnits / 2) * pixelUnitsRatio - slider_image_inside_rect)) {
             return;
           }
 
           dragPin.attr('x', newX);
-          rect.attr('width', newX + (slider_image_width * slider_image_inside_rect_percentage / 100));
+          rect.attr('width', newX + slider_image_inside_rect);
+
+          var newValue = getCurrentValue();
+          newValue = Math.round(newValue); //Number(newValue.toFixed(2));
+
           if (typeof config.slideHandler === 'function') {
-            config.slideHandler(getCurrentValue());
+            config.slideHandler(newValue);
           }
         }
 
         function getCurrentValue() {
-          return rect.attr('width') / pixelUnitsRatio
+          return (rect.attr('width') / pixelUnitsRatio - paddingUnits / 2) + minValue;
         }
 
         function dragStartHandler() {
@@ -237,20 +361,41 @@ angular.module('app')
         }
 
         function showMinMaxRect() {
-          minRect.style('opacity', min_max_rect_opacity);
-          maxRect.style('opacity', min_max_rect_opacity);
+          minRect.style('opacity', minMaxRectVisibleOpacity);
+          maxRect.style('opacity', minMaxRectVisibleOpacity);
           minBracket.style('opacity', 0.4);
           maxBracket.style('opacity', 0.4);
         }
 
         function hideMinMaxRect() {
-          minRect.style('opacity', min_max_rect_default_opacity);
-          maxRect.style('opacity', min_max_rect_default_opacity);
-          minBracket.style('opacity', min_max_rect_default_opacity);
-          maxBracket.style('opacity', min_max_rect_default_opacity);
+          minRect.style('opacity', minMaxRectDefaultOpacity);
+          maxRect.style('opacity', minMaxRectDefaultOpacity);
+          minBracket.style('opacity', minMaxRectDefaultOpacity);
+          maxBracket.style('opacity', minMaxRectDefaultOpacity);
+        }
+
+        function appendVerticalLines() {
+          svg.select('.lines').remove();
+          var lines = svg.append('g')
+            .attr('class', 'lines');
+          var width = parseInt(svg.style('width'), 10);
+          var height = parseInt(svg.style('height'), 10);
+          var currentPos = 0;
+          while (currentPos < width) {
+            lines.append("line")
+              .attr("x1", currentPos)
+              .attr("x2", currentPos)
+              .attr("y1", 0)
+              .attr("y2", height)
+              .style("stroke-dasharray", "4,4")//dashed array for line
+              .style("stroke", verticalLinesColor)
+            currentPos += verticalLinesSeparation;
+          }
+          return lines;
         }
 
         drag = d3.behavior.drag()
+          .origin(Object)
           .on('drag', dragHandler)
           .on('dragstart', dragStartHandler)
           .on('dragend', dragEndHandler);
